f6832cf50596c8fe1ab6a7b3ee6d7a255e12064a
gbnegrini <negrini.guilherme@gmail.com>
2020-04-05 20:58:42 -0300

    First commit

---
+++ app/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const speech = require("@google-cloud/speech");
+
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+
+async function transcribe(config, audio) {
+  const client = new speech.SpeechClient();
+  const request = {
+    config: config,
+    audio: audio,
+  };
+
+  // Detects speech in the audio file
+  const [operation] = await client.longRunningRecognize(request);
+  const [response] = await operation.promise();
+  const transcription = response.results
+    .map((result) => result.alternatives[0].transcript)
+    .join("\n");
+  console.log(`Transcription: ${transcription}`);
+}
+
+app.post("/audio", (req, res) => {
+  res.sendStatus(200);
+  console.log('Transcribing...');
+  transcribe(req.body.config, req.body.audio);
+});
+
+app.get("/", (req, res) => {
+  return console.log("Working");
+});
+
+app.listen(port);
+console.log(`Server runing at: ${require("os").hostname()}/${port}`);
---
+++ cloud-functions/triggerBucket.js
@@ -0,0 +1,41 @@
+/**
+ * This function is implemented as a Google Cloud Function
+ *
+ * Triggered from a change to a Cloud Storage bucket.
+ *
+ * @param {!Object} event Event payload.
+ * @param {!Object} context Metadata for the event.
+ */
+const axios = require("axios");
+const containerAddress = "YOUR_CONTAINER_ADDRESS";
+
+const formatUri = (event) => {
+  return `gs://${event.bucket}/${event.name}`;
+};
+
+exports.triggerBucket = (event, context) => {
+  const gcsEvent = event;
+  if (gcsEvent.contentType.includes("audio") || gcsEvent.name.includes(".raw")) {
+    console.log("Audio file detected");
+    axios
+      .post(`${containerAddress}/audio`, {
+        config: {
+          encoding: "LINEAR16",
+          sampleRateHertz: 16000,
+          languageCode: "en-US"
+        },
+        audio: {
+          uri: formatUri(gcsEvent),
+        }
+      })
+      .then((res) => {
+        console.log(`Response status: ${res.statusCode}`);
+        console.log(res);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  } else {
+    console.log("Not an audio file.");
+  }
+};
